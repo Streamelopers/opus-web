d0b4944725eed69b064baebbfeccbce563d0c404
Danny Feliz <dannyfeliz08@gmail.com>
2021-01-30 22:36:49 -0400

    Generate 50 jobs

--- pages/api/jobs.tsx
+++ pages/api/jobs.tsx
@@ -1,8 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
 import jobFactory from "factories/jobFactory";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const jobsFakeResponse = new Array(25).fill(null).map(() => jobFactory());
+  const jobsFakeResponse = new Array(50).fill(null).map(() => jobFactory());
+  fs.writeFileSync(path.resolve(__dirname, "../../"));
   res.status(200).json(jobsFakeResponse);
 };
