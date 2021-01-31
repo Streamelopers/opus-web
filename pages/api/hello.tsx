3a983eec29d4eb032c5bf13103ee4b3f32833598
Danny Feliz <dannyfeliz08@gmail.com>
2021-01-30 22:39:10 -0400

    Rename the api back from jobs to hello

---
+++ pages/api/hello.tsx
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({
+    hello: "world"
+  });
+};
