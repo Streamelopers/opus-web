95db4c0f3dc8cb59bac88af3d3a7d88111a8d517
Danny Feliz <dannyfeliz08@gmail.com>
2021-02-03 03:07:40 -0400

    Add sign-in schema

---
+++ schemas/sign-in.schema.tsx
@@ -0,0 +1,5 @@
+import * as yup from "yup";
+export const signInForm = yup.object().shape({
+  email: yup.string().email().trim().label("El Correo"),
+  password: yup.string().required().label("La Contraseña")
+});
