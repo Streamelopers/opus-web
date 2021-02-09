994e2ee21ed713ebf0b96807f64a97b9ed3105fb
Danny Feliz <dannyfeliz08@gmail.com>
2021-02-09 04:52:59 -0400

    Add page metadata

--- pages/registro/index.tsx
+++ pages/registro/index.tsx
@@ -19,6 +19,7 @@ import { Formik, Form, Field, FieldProps, FormikProps, FormikHelpers } from "for
 import { signUpForm } from "../../schemas/sign-up.schema";
 import { ValidationError } from "yup";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import Page from "@/components/Page";
 interface FormValues {
   name: string;
   email: string;
@@ -50,76 +51,78 @@ const SignUpForm = () => {
   }
 
   return (
-    <Formik initialValues={formValues} onSubmit={handleSubmit}>
-      {(props: FormikProps<FormValues>) => (
-        <Flex justifyContent="center" mt="25">
-          <Stack maxW="500px">
-            <Heading fontWeight="700">Registro</Heading>
-            <Divider />
-            <Text fontSize="xl">Regístrate en la plataforma para poder empezar a publicar empleos</Text>
-            <Form>
-              <Stack spacing="20px" mt="5">
-                <Field name="name">
-                  {({ field, form }: FieldProps) => (
-                    <FormControl isInvalid={!!form.errors.name && !!form.touched.name} isRequired>
-                      <FormLabel htmlFor="name">Nombre</FormLabel>
-                      <Input {...field} id="name" placeholder="Juan Perez" autoComplete="name" />
-                      <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                </Field>
+    <Page title="Regsitro" description="Formulario de registro">
+      <Formik initialValues={formValues} onSubmit={handleSubmit}>
+        {(props: FormikProps<FormValues>) => (
+          <Flex justifyContent="center" mt="25">
+            <Stack maxW="500px">
+              <Heading fontWeight="700">Registro</Heading>
+              <Divider />
+              <Text fontSize="xl">Regístrate en la plataforma para poder empezar a publicar empleos</Text>
+              <Form>
+                <Stack spacing="20px" mt="5">
+                  <Field name="name">
+                    {({ field, form }: FieldProps) => (
+                      <FormControl isInvalid={!!form.errors.name && !!form.touched.name} isRequired>
+                        <FormLabel htmlFor="name">Nombre</FormLabel>
+                        <Input {...field} id="name" placeholder="Juan Perez" autoComplete="name" />
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
 
-                <Field name="email">
-                  {({ field, form }: FieldProps) => (
-                    <FormControl isInvalid={!!form.errors.email && !!form.touched.email} isRequired>
-                      <FormLabel htmlFor="email">Correo</FormLabel>
-                      <Input {...field} type="email" autoComplete="email" id="email" placeholder="juan@ejemplo.com" />
-                      <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                </Field>
+                  <Field name="email">
+                    {({ field, form }: FieldProps) => (
+                      <FormControl isInvalid={!!form.errors.email && !!form.touched.email} isRequired>
+                        <FormLabel htmlFor="email">Correo</FormLabel>
+                        <Input {...field} type="email" autoComplete="email" id="email" placeholder="juan@ejemplo.com" />
+                        <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
 
-                <Field name="password">
-                  {({ field, form }: FieldProps) => (
-                    <FormControl isInvalid={!!form.errors.password && !!form.touched.password} isRequired>
-                      <FormLabel htmlFor="password">Contraseña</FormLabel>
-                      <Input
-                        {...field}
-                        type={showPassword ? "text" : "password"}
-                        autoComplete="new-password"
-                        id="password"
-                        placeholder="******"
-                      />
-                      <InputRightElement width="3rem" css={{ top: "37.5px" }}>
-                        <Button h="1.75rem" size="sm" onClick={() => setShowPassword(!showPassword)}>
-                          {showPassword ? <Icon as={FaEye} /> : <Icon as={FaEyeSlash} />}
-                        </Button>
-                      </InputRightElement>
-                      <FormErrorMessage>{form.errors.password}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                </Field>
+                  <Field name="password">
+                    {({ field, form }: FieldProps) => (
+                      <FormControl isInvalid={!!form.errors.password && !!form.touched.password} isRequired>
+                        <FormLabel htmlFor="password">Contraseña</FormLabel>
+                        <Input
+                          {...field}
+                          type={showPassword ? "text" : "password"}
+                          autoComplete="new-password"
+                          id="password"
+                          placeholder="******"
+                        />
+                        <InputRightElement width="3rem" css={{ top: "37.5px" }}>
+                          <Button h="1.75rem" size="sm" onClick={() => setShowPassword(!showPassword)}>
+                            {showPassword ? <Icon as={FaEye} /> : <Icon as={FaEyeSlash} />}
+                          </Button>
+                        </InputRightElement>
+                        <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
 
-                <Button mt={4} colorScheme="blue" type="submit" isLoading={props.isSubmitting}>
-                  Registrarse
-                </Button>
+                  <Button mt={4} colorScheme="blue" type="submit" isLoading={props.isSubmitting}>
+                    Registrarse
+                  </Button>
 
-                <Divider mt="5" />
+                  <Divider mt="5" />
 
-                <Flex justifyContent="center" alignItems="center" w="100%">
-                  <Text>Si ya tienes cuenta puedes</Text>
-                  <NextLink href="/iniciar-sesion">
-                    <Link ml="1" color="blue.300">
-                      iniciar sesión aquí
-                    </Link>
-                  </NextLink>
-                </Flex>
-              </Stack>
-            </Form>
-          </Stack>
-        </Flex>
-      )}
-    </Formik>
+                  <Flex justifyContent="center" alignItems="center" w="100%">
+                    <Text>Si ya tienes cuenta puedes</Text>
+                    <NextLink href="/iniciar-sesion">
+                      <Link ml="1" color="blue.300">
+                        iniciar sesión aquí
+                      </Link>
+                    </NextLink>
+                  </Flex>
+                </Stack>
+              </Form>
+            </Stack>
+          </Flex>
+        )}
+      </Formik>
+    </Page>
   );
 };
 
