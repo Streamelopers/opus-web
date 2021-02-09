51fc8945248e00b508af8c621807629968c6d693
Danny Feliz <dannyfeliz08@gmail.com>
2021-02-09 04:53:49 -0400

    Add job details

--- pages/job/[id]/index.tsx
+++ pages/job/[id]/index.tsx
@@ -1,251 +1,110 @@
-import React from "react";
-import JobHeader from "@/components/JobDetails/JobHeader/JobHeader";
-import JobInfo from "@/components/JobDetails/JobInfo/JobInfo";
-import { Badge, Box, Button, Divider, Text, Icon, Flex, Link } from "@chakra-ui/react";
-import { FaFacebook, FaTwitter, FaWhatsapp, FaLinkedinIn } from "react-icons/fa";
-import NextLink from "next/link";
+import Page from "@/components/Page";
+import ShareButtons from "@/components/ShareButtons";
+import JobHeader from "@/components/JobHeader";
+import JobInfo from "@/components/JobInfo";
+import { Box, Divider, Text } from "@chakra-ui/react";
+import { GetStaticPropsContext } from "next";
+import React, { FC } from "react";
+import { Job } from "types/Job";
+import jobsData from "../../../data/jobs.json";
+import HowToApply from "@/components/HowToApply";
+import RelatedJobs from "@/components/RelatedJobs";
+import Skills from "@/components/Skills";
 
-import { FacebookShareButton, LinkedinShareButton, TwitterShareButton, WhatsappShareButton } from "react-share";
-import Image from "next/image";
+type JobProps = {
+  job: Job;
+};
 
-const JobDetails = () => {
-  const someHTML = `
-    <p>You will work directly with the founder of a young company called Polymer Search and work on some exciting
-     search innovation. I have been a Computer science engineer myself for the last 20+ years (ex-Google).
-      I am looking for an experienced full stack developer. The focus is on data analytics and
-       management via a SaaS webapp via a fairly involved UX. Some of the skills I am looking for are:</p>
-      <br>
-      <h1>Hello</h1>
-      <ul>
-        <li>5+ years experience in performant Javascript for frontend and backend.</li>
-
-        <li>2+ year Vue.js experience (or another Reactive framework like React and willing to pickup Vue quickly)</li>
-
-        <li>Preferable: <a href="#">Node.js</a> experience: Writing performant web applications, working with popular frameworks.</li>
-
-        <li>Comfortable with HTML/CSS and CSS frameworks like Bootstrap</li>
-
-        <li>Comfortable using Chrome or Firefox Dev Tools to debug and instrument web applications</li>
-
-        <li>Working with databases : Postgres/Redis/Mongodb</li>
-
-        <li>Working with different web architectures and technologies (Load balancing, performance instrumentation, logging, queuing technologies, databases)</li>
-      </ul>
-  `;
-
+const JobDetails: FC<JobProps> = ({ job }: JobProps) => {
   return (
-    <>
-      <JobHeader />
-      <JobInfo />
+    <Page>
+      <>
+        <JobHeader
+          location={job.Location.Name}
+          picture={job.Company.Picture}
+          title={job.Title}
+          website={job.Company.Website}
+        />
+        <JobInfo
+          createdAt={job.CreatedAt}
+          currencyCode={job.Currency?.ISOCode}
+          isRemote={job.IsRemote}
+          isRemoteOnly={job.IsRemoteOnly}
+          jobType={job.JobType.Name}
+          location={job.Location.Name}
+          maxSalary={job.MaxSalary}
+          minSalary={job.MinSalary}
+        />
 
-      <Box mt="5" className="job-description">
-        <Text fontWeight="600" fontSize="2xl">
-          Job description
-        </Text>
-        <Divider />
-        <Box>
-          <div dangerouslySetInnerHTML={{ __html: someHTML }} />
-        </Box>
-      </Box>
-
-      <Box>
-        <Text fontWeight="600" fontSize="xl">
-          Skills
-        </Text>
-        <Divider />
-        <Box mt="1">
-          <Badge colorScheme="blue" padding="1" mr="2" borderRadius="10px">
-            Vue
-          </Badge>
-          <Badge colorScheme="blue" padding="1" mr="2" borderRadius="10px">
-            React
-          </Badge>
-          <Badge colorScheme="blue" padding="1" mr="2" borderRadius="10px">
-            Nuxt
-          </Badge>
-        </Box>
-      </Box>
-
-      <Box bg="gray.100" borderRadius="10px" p="5" mt="5">
-        <Text fontWeight="600" fontSize="2xl">
-          How to apply
-        </Text>
-        <Text>
-          Find out more about us on our Careers page and submit your application through the job opening posted there:
-          https://careers.jibblegroup.com/p/dd9322f4001f01-senior-front-end-developer
-        </Text>
-
-        <Button colorScheme="teal" size="md" mt="3">
-          Apply now!
-        </Button>
-      </Box>
-
-      <Box mt="5">
-        <Text>Do you think this job matches someone? Share!</Text>
-        <Box>
-          <FacebookShareButton quote="abc" url="http://google.com" hashtag="#trabajo" style={{ marginRight: "10px" }}>
-            <Badge variant="outline" colorScheme="facebook">
-              <Flex alignItems="center">
-                <Icon as={FaFacebook} />
-                &nbsp;Facebook
-              </Flex>
-            </Badge>
-          </FacebookShareButton>
-
-          <TwitterShareButton
-            title="this is the title"
-            url="http://google.com"
-            via="opus"
-            style={{ marginRight: "10px" }}
-          >
-            <Badge variant="outline" colorScheme="twitter">
-              <Flex alignItems="center">
-                <Icon as={FaTwitter} />
-                &nbsp;Twitter
-              </Flex>
-            </Badge>
-          </TwitterShareButton>
-
-          <LinkedinShareButton
-            title="this is the title"
-            summary="some summary"
-            source="NYT"
-            url="http://google.com"
-            style={{ marginRight: "10px" }}
-          >
-            <Badge variant="outline" colorScheme="linkedin">
-              <Flex alignItems="center">
-                <Icon as={FaLinkedinIn} />
-                &nbsp;LinkedIn
-              </Flex>
-            </Badge>
-          </LinkedinShareButton>
-
-          <WhatsappShareButton title="this is the title" url="http://google.com">
-            <Badge variant="outline" colorScheme="whatsapp">
-              <Flex alignItems="center">
-                <Icon as={FaWhatsapp} />
-                &nbsp;WhatsApp
-              </Flex>
-            </Badge>
-          </WhatsappShareButton>
-        </Box>
-      </Box>
-
-      <Box mt="5">
-        <Text fontSize="2xl" fontWeight="600">
-          Looking for more full-time jobs?
-        </Text>
-        <Divider />
-
-        <Box
-          mt="5"
-          padding="5px 10px"
-          borderRadius="10px"
-          _hover={{
-            bg: "gray.50"
-          }}
-        >
-          <Flex alignItems="center">
-            <Box p="2">
-              <Image
-                src="https://vuejobs.com/storage/avatars/yBYKiaPkbln6yDt4z0kA4sUikiCsq5JQRYc5PU0y.png"
-                width="50"
-                height="50"
-              />
-            </Box>
-            <Box flex="1">
-              <Text>
-                <NextLink href={`/job/123`}>
-                  <Link color="blue.400">Senior Frontend Developer (VueJS)</Link>
-                </NextLink>
-              </Text>
-              <Text>
-                <Text as="span" color="gray.400">
-                  at
-                </Text>
-                <Text as="strong"> CyberCoder </Text>
-                <Text as="span">Washington, DC, USA - Washington, DC, USA</Text>
-              </Text>
-            </Box>
-            <Box>
-              <Text>1 weak ago</Text>
-            </Box>
-          </Flex>
-        </Box>
-      </Box>
-
-      <Box
-        padding="5px 10px"
-        borderRadius="10px"
-        _hover={{
-          bg: "gray.50"
-        }}
-      >
-        <Flex alignItems="center">
-          <Box p="2">
-            <Image
-              src="https://vuejobs.com/storage/avatars/yBYKiaPkbln6yDt4z0kA4sUikiCsq5JQRYc5PU0y.png"
-              width="50"
-              height="50"
-            />
-          </Box>
-          <Box flex="1">
-            <Text>
-              <NextLink href={`/job/123`}>
-                <Link color="blue.400">Senior Frontend Developer (VueJS)</Link>
-              </NextLink>
-            </Text>
-            <Text>
-              <Text as="span" color="gray.400">
-                at
-              </Text>
-              <Text as="strong"> CyberCoder </Text>
-              <Text as="span">Washington, DC, USA - Washington, DC, USA</Text>
-            </Text>
-          </Box>
+        <Box mt="5" className="job-description">
+          <Text fontWeight="600" fontSize="2xl">
+            Descripción de la compañia
+          </Text>
+          <Divider mt="2" mb="3" />
           <Box>
-            <Text>1 weak ago</Text>
+            <div dangerouslySetInnerHTML={{ __html: job.Company.Description }} />
           </Box>
-        </Flex>
-      </Box>
+        </Box>
 
-      <Box
-        padding="5px 10px"
-        borderRadius="10px"
-        _hover={{
-          bg: "gray.50"
-        }}
-      >
-        <Flex alignItems="center">
-          <Box p="2">
-            <Image
-              src="https://vuejobs.com/storage/avatars/yBYKiaPkbln6yDt4z0kA4sUikiCsq5JQRYc5PU0y.png"
-              width="50"
-              height="50"
-            />
-          </Box>
-          <Box flex="1">
-            <Text>
-              <NextLink href={`/job/123`}>
-                <Link color="blue.400">Senior Frontend Developer (VueJS)</Link>
-              </NextLink>
-            </Text>
-            <Text>
-              <Text as="span" color="gray.400">
-                at
-              </Text>
-              <Text as="strong"> CyberCoder </Text>
-              <Text as="span">Washington, DC, USA - Washington, DC, USA</Text>
-            </Text>
-          </Box>
+        <Box mt="5" className="job-description">
+          <Text fontWeight="600" fontSize="2xl">
+            Descripción del trabajo
+          </Text>
+          <Divider mt="2" mb="3" />
           <Box>
-            <Text>1 weak ago</Text>
+            <div dangerouslySetInnerHTML={{ __html: job.Description }} />
           </Box>
-        </Flex>
-      </Box>
-    </>
+        </Box>
+
+        <Skills />
+
+        <HowToApply howToApply={job.HowToApply} applicationTarget={"http://google.com"} />
+
+        <ShareButtons title={job.Title} description={job.Description} />
+
+        <RelatedJobs />
+      </>
+    </Page>
   );
 };
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  interface paramsType {
+    params: {
+      id: string;
+    };
+  }
+
+  const params: Array<paramsType> = [];
+  // TODO: Fix this later
+  // @ts-ignore
+  const list: Array<Job> = jobsData;
+
+  list.forEach((job: Job) => {
+    params.push({
+      params: { id: job.Id.toString() }
+    });
+  });
+
+  return { paths: params, fallback: false };
+}
+
+// This also gets called at build time
+export async function getStaticProps(context: GetStaticPropsContext) {
+  // const { origin } = absoluteUrl(context.e, "localhost:3000");
+  // const resp = await fetch(`${origin}/api/users`);
+  // const users = await resp.json();
+  // return { users };
+
+  // TODO: Fix this later
+  // @ts-ignore
+  const job = jobsData.find((job: Job) => job.Id.toString() === context.params?.id);
+  return {
+    props: {
+      job
+    }
+  };
+}
 
 export default JobDetails;
