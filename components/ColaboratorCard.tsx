0a3884601f8bdffa875169f24a84bd03c3cb8a90
unknown <jhonas724@gmail.com>
2021-10-16 01:34:36 -0400

    set anchor element around the colaborator username use for navigate to their github profile, ColaboratorCard css module added

--- components/ColaboratorCard.tsx
+++ components/ColaboratorCard.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { Colaborator } from "@/types/Colaborator";
-import { Badge, Box, Image } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
+import styles from "./ColaboratorCard.module.css";
 
 type ColaboratorCardProps = {
   colaborator: Colaborator;
@@ -12,7 +13,9 @@ const ColaboratorCard: FC<ColaboratorCardProps> = ({ colaborator }) => {
       <Image width="100%" src={colaborator.avatar_url} alt={colaborator.login} />
       <Box p="6">
         <Box mt="1" fontWeight="bold" as="h1" lineHeight="tight" isTruncated textAlign="center">
-          {colaborator.login.toUpperCase()}
+          <a className={styles["name-link"]} href={colaborator.html_url} target="_blank">
+            {colaborator.login.toUpperCase()}
+          </a>
         </Box>
         <Box textAlign="center">{colaborator.contributions} contribuciones</Box>
       </Box>
