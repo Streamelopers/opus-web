25c14f15407e19f888335d93c2694e6e59eea5b9
Jadhiel Vélez <jadhielv@gmail.com>
2021-11-07 23:58:45 -0400

    Fix linting errors

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,1 @@
-export * from './useWindowSize';
+export * from "./useWindowSize";
--- src/utils/helperFn.js
+++ src/utils/helperFn.js
@@ -1,4 +1,4 @@
-export const titleCase = str => {
+export const titleCase = (str) => {
   var splitStr = str.toLowerCase().split(" ");
   for (var i = 0; i < splitStr.length; i++) {
     // You do not need to check if i is larger than splitStr length, as your for does that for you
