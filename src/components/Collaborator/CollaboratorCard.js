e1f48375a35d5bd8e617ac31eeea70ccf5ac9183
Jadhiel Vélez <jadhielv@gmail.com>
2021-12-13 17:01:50 -0400

    Fix linting errors

--- src/components/Collaborator/CollaboratorCard.js
+++ src/components/Collaborator/CollaboratorCard.js
@@ -1,25 +1,33 @@
-import React from "react"
+import React from "react";
 
+const CollaboratorCard = ({ collaborator }) => {
+  return (
+    <div
+      className="bg-white pt-9 pb-7 rounded-4 p-2 mb-9 feature-cardOne-adjustments border"
+      style={{ borderColor: "#F6F7FB", height: "100%", width: "auto" }}
+    >
+      <div className="mb-7 d-flex justify-content-center">
+        <a target="_blank" href={collaborator.html_url}>
+          <img src={collaborator.avatar_url} width={"100%"} alt="" />
+        </a>
+      </div>
+      <h2 className="mt-n4">
+        <p className="text-center">
+          <a
+            target="_blank"
+            href={collaborator.html_url}
+            className="font-size-7 text-black-2 font-weight-bold mb-4"
+          >
+            {collaborator.login}
+          </a>
+        </p>
+      </h2>
+      <p className="text-center font-size-4">
+        {collaborator.contributions} contribuciones
+      </p>
+      <hr className="bg-light border-2 border-top border-light" />
+    </div>
+  );
+};
 
-const CollaboratorCard = ({collaborator}) => {
-    return (
-        <div className="bg-white pt-9 pb-7 rounded-4 p-2 mb-9 feature-cardOne-adjustments border" style={{borderColor: "#F6F7FB", height:"100%", width: "auto"}}>
-            <div className="mb-7 d-flex justify-content-center">
-                <a target="_blank" href={collaborator.html_url}>
-                    <img src={collaborator.avatar_url}  width={"100%"} alt="" />
-                </a>   
-            </div>
-            <h2 className="mt-n4">
-                <p  className="text-center"><a target="_blank" href={collaborator.html_url} className="font-size-7 text-black-2 font-weight-bold mb-4">
-                    {collaborator.login}
-                </a></p>
-            </h2>
-            <p className="text-center font-size-4">             
-                    {collaborator.contributions} contribuciones
-            </p>
-            <hr className="bg-light border-2 border-top border-light"/>
-        </div>
-    )
-}
-
-export default CollaboratorCard
+export default CollaboratorCard;
