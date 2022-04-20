7faf9dd93e8f91f3161bb1b0c7f957dba0d59c16
Jadhiel Vélez <jadhielv@gmail.com>
2022-04-20 19:18:31 -0400

    Disable static imports

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withFonts = require("next-fonts");
 
 module.exports = withFonts(
   withOptimizedImages({
-    /* config for next-optimized-images */
-    // your config for other plugins or the general next.js here...
+    images: {
+      disableStaticImages: true,
+    },
   })
 );
